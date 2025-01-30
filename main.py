e99bfc0959c88a84f77d56cfb3bdf9bef8bda2dc
paulcostanza <paulvincentcostanza@gmail.com>
2025-01-29 20:49:30 -0500

    added groups to main

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
     clock = p.time.Clock()
     dt = 0
 
+    # groups
+    updateable = p.sprite.Group()
+    drawable = p.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     # our player
     player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -21,13 +26,15 @@ def main():
         for event in p.event.get():
             if event.type == p.QUIT:
                 return
-        ######
+        # # # # # # # # # # # # # # # # # # # 
 
         p.Surface.fill(screen, (0,0,0))
 
         # reads user's input
-        player_1.update(dt)
-        player_1.draw(screen)
+        updateable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
         p.display.flip()
 
         dt = clock.tick(60) / 1000
